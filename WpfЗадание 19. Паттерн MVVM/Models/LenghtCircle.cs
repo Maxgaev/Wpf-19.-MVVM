896bfff6f4a52d88c5b049734635c0f429d6549c
Максим Алехин <Max-crf@mail.ru>
2022-02-27 15:47:18 +0700

    Добавьте файлы проекта.

---
+++ WpfЗадание 19. Паттерн MVVM/Models/LenghtCircle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfЗадание_19.Паттерн_MVVM.Models
+{
+    static class LengthCircle
+    {
+        public static double Add(double r) => Math.Round((2 * Math.PI * r), 2);
+
+    }
+}
---
+++ WpfЗадание 19. Паттерн MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfЗадание_19.Паттерн_MVVM.Models;
+
+namespace WpfЗадание_19.Паттерн_MVVM.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double lenght;
+        public double Length
+        {
+            get => lenght;
+            set
+            {
+                lenght = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Length = LengthCircle.Add(Radius);
+        }
+
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (radius !=0)
+                return true;
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+
+    }
+}
